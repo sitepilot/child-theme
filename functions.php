4110321e4177918241ddb3552408a0525a93f0e2
Nick Jansen <n.jansen@sitepilot.io>
2019-12-11 18:50:44 +0100

    Fix composer autoload when using WPCLI

--- functions.php
+++ functions.php
@@ -11,16 +11,15 @@ if (defined('SITEPILOT_THEME_VERSION')) {
     return;
 }
 
-// Require Composer autoloader if it exists
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require_once 'vendor/autoload.php';
-}
-
 // Useful global constants
 define('SITEPILOT_THEME_VERSION', '{{SP_VERSION}}');
 define('SITEPILOT_THEME_DIR', trailingslashit(get_stylesheet_directory()));
 define('SITEPILOT_THEME_URL', trailingslashit(get_stylesheet_directory_uri()));
 define('SITEPILOT_THEME_FILE', SITEPILOT_THEME_DIR . 'functions.php');
 
+// Require Composer autoloader if it exists
+if (file_exists(SITEPILOT_THEME_DIR . '/vendor/autoload.php')) {
+    require_once SITEPILOT_THEME_DIR . 'vendor/autoload.php';
+}
+
 Sitepilot\Theme\Theme::init();
- 
